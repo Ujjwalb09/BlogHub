a34de84ab979cb22679c38707816cfa280071f89
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-04-10 20:29:20 +0530

    configured appwrite database and queries to upload files etc...

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import config from "../config/config";
+import conf from "../conf/conf";
 
 import { Client, Account, ID } from "appwrite";
 
@@ -9,8 +9,8 @@ export class AuthService {
 
     constructor(){
         this.client
-            .setEndpoint(config.appwriteUrl)
-            .setProject(appwriteProjectId);
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client)
     }
 
