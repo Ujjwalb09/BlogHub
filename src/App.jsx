b5bf72abd9b517521d2b673acbc9f514336e7465
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-04-12 16:55:28 +0530

    configured store using redux toolkit  and made few components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import { useDispatch } from 'react-redux';
 import './App.css'
+import { useEffect, useState } from 'react';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import {Footer, Header} from './components'
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
- 
-  return (
-    <>
-      <h1>Blog App</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+         TODO: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (<div className='min-h-sc'> Loading......</div>)
 }
 
 export default App
